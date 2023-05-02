b334e6e47532d42a1ab483a01adc2805cb726ff0
dhanasekaran <dhanasekaranm@zeta.tech>
2023-05-02 22:37:32 +0530

    Setup

---
+++ MVVM+CleanArchitecture/MVVM_CleanArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_CleanArchitectureApp.swift
+//  MVVM+CleanArchitecture
+//
+//  Created by DM on 02/05/23.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_CleanArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
